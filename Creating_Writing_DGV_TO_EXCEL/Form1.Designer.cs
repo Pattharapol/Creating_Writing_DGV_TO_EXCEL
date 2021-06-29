0cf8dc4d54190d9ae254c6edae7417e30078af18
unknown <tjtik83@gmail.com>
2021-06-29 09:58:50 +0700

    completed

---
+++ Creating_Writing_DGV_TO_EXCEL/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace Creating_Writing_DGV_TO_EXCEL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.btnExportExcel = new System.Windows.Forms.Button();
+            this.txtSQL = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRun = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvData
+            // 
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Location = new System.Drawing.Point(12, 245);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.Size = new System.Drawing.Size(919, 375);
+            this.dgvData.TabIndex = 0;
+            // 
+            // btnExportExcel
+            // 
+            this.btnExportExcel.Location = new System.Drawing.Point(820, 205);
+            this.btnExportExcel.Name = "btnExportExcel";
+            this.btnExportExcel.Size = new System.Drawing.Size(111, 34);
+            this.btnExportExcel.TabIndex = 1;
+            this.btnExportExcel.Text = "Export Excel";
+            this.btnExportExcel.UseVisualStyleBackColor = true;
+            this.btnExportExcel.Click += new System.EventHandler(this.btnExportExcel_Click);
+            // 
+            // txtSQL
+            // 
+            this.txtSQL.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.txtSQL.Location = new System.Drawing.Point(38, 20);
+            this.txtSQL.Multiline = true;
+            this.txtSQL.Name = "txtSQL";
+            this.txtSQL.Size = new System.Drawing.Size(776, 219);
+            this.txtSQL.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "sql";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(820, 20);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(111, 34);
+            this.btnRun.TabIndex = 4;
+            this.btnRun.Text = "&RUN";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 632);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSQL);
+            this.Controls.Add(this.btnExportExcel);
+            this.Controls.Add(this.dgvData);
+            this.Name = "Form1";
+            this.Text = "Export DataGridVIew To Excel";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.Button btnExportExcel;
+        private System.Windows.Forms.TextBox txtSQL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRun;
+    }
+}
+
---
+++ Creating_Writing_DGV_TO_EXCEL/Form1.cs
@@ -0,0 +1,75 @@
+﻿using MySql.Data.MySqlClient;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Creating_Writing_DGV_TO_EXCEL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // first, Adding EPPPlus from nugat packages
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private DataTable dt = new DataTable();
+
+        private async Task<DataTable> loadData()
+        {
+            MySqlConnection conn = new MySqlConnection("server=192.168.0.2;userid=root;password=boom123;");
+            MySqlDataAdapter da = new MySqlDataAdapter(txtSQL.Text, conn);
+            conn.Open();
+
+            await Task.Run(() => da.Fill(dt));
+
+            return dt;
+        }
+
+        private void btnExportExcel_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Excel Workbook|*.xlsx";
+            try
+            {
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    var fileInfo = new FileInfo(sfd.FileName);
+                    ExcelPackage excelPackage = new ExcelPackage(fileInfo);
+                    ExcelWorksheet workSheet = excelPackage.Workbook.Worksheets.Add("sheet1");
+                    workSheet.Cells.LoadFromDataTable(dt, true, OfficeOpenXml.Table.TableStyles.None);
+                    excelPackage.Save();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            MessageBox.Show("เรียบร้อยครับ");
+        }
+
+        private async void btnRun_Click(object sender, EventArgs e)
+        {
+            if (dt.Rows.Count < 0 || dt == null)
+            {
+                MessageBox.Show("ตรวจสอบ sql string อีกครั้ง");
+            }
+            else
+            {
+                dgvData.DataSource = await loadData();
+            }
+        }
+    }
+}
